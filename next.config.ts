0827847b9dd8f264d128479f3727aec0539ca8b2
Prashant Kumar <mrsheoganj@gmail.com>
2026-01-29 16:09:24 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://photos.

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'photos.fife.usercontent.google.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
